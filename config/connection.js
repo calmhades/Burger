e9a283e4756817748aebfffbf2f23178236e6a32
calm hades <calm.hades@gmail.com>
2018-10-30 00:35:25 -0500

    2

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require('mysql');
 
+// var JAWSDB_URL = "mysql://uruxw3372uqmwxy5:irt7kvdr057i47z7@tj5iv8piornf713y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zohmdq37cdrsai5r"
+
+if (process.env.mysql.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
 var connection = mysql.createConnection({
-    host: "tj5iv8piornf713y.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "uruxw3372uqmwxy5",
-    password: "irt7kvdr057i47z7",
-    database: "zohmdq37cdrsai5r"
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
 });
+};
 
 connection.connect(function(err) {
     if (err) {
